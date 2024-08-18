c1247010b99b15d122f1eeb423c8ffacbd588acd
akshansh14 <akshanshsj14@gmail.com>
2024-08-18 22:16:01 +0530

    Router Error Debug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MyCalendar from './Pages/MyCalendar';
-import EventList from './Pages/EventList';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyCalendar from "./Pages/MyCalendar";
+import EventList from "./Pages/EventList";
+import { EventProvider } from "./Context/EventContext";
 
 function App() {
   return (
     <div className="App">
-       <Routes>
-        <Route path="/" element={<MyCalendar />} />
-        <Route path="/events" element={<EventList/>} />
-      </Routes>
+      <EventProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MyCalendar />} />
+            <Route path="/events" element={<EventList />} />
+          </Routes>
+        </Router>
+      </EventProvider>
     </div>
   );
 }
